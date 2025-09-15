2b737f181291f348ce3a9babef24d6e084e1211a
Daler Sharifkulov <dsarifkulo021@gmail.com>
2025-09-15 17:22:09 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
 	const p = useTranslations("ProductsCategories");
 
 	const [openModal, setOpenModal] = useState<"feedback" | "vacancies" | null>(null);
+	
 
 	return (
 		<main>
